e0966e3eee57f5ca71e39bc4e5b94d6f7677cd99
joneyin <yin_jone@163.com>
2017-12-20 11:07:05 +0800

    First Upload

---
+++ DFFramework.framework/Headers/DFAdditionOperation.h
@@ -0,0 +1,13 @@
+//
+//  DFAdditionOperation.h
+//  DFFramework
+//
+//  Created by Jone Yin on 2017/12/20.
+//  Copyright © 2017年 Gotell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DFAdditionOperation : NSObject
+-(void)addNumber:(int)oneNum Num:(int)twoNum;
+@end
---
+++ DFFramework.framework/Headers/DFFramework.h
@@ -0,0 +1,19 @@
+//
+//  DFFramework.h
+//  DFFramework
+//
+//  Created by Jone Yin on 2017/12/20.
+//  Copyright © 2017年 Gotell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DFFramework.
+FOUNDATION_EXPORT double DFFrameworkVersionNumber;
+
+//! Project version string for DFFramework.
+FOUNDATION_EXPORT const unsigned char DFFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DFFramework/PublicHeader.h>
+#import <DFFramework/DFAdditionOperation.h>
+
